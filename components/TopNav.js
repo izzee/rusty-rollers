47bc993f5941a93ec7ca74bd23be55d8a2689885
Isaac <ichabon@me.com>
2022-08-16 16:07:59 -0400

    add social icons

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,4 +1,5 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import Image from 'next/image'
 import styles from '../styles/TopNav.module.scss'
 
 
@@ -6,8 +7,13 @@ const TopNav = () => {
 
   return (
     <div className={styles.topnav}>
-      <ConnectButton />
+      <ConnectButton />    
       <div className={styles.socials}>
+        <Image src='/images/twitter.svg' width='40px' height='40px'/>
+        <Image src='/images/opensea.svg' width='40px' height='40px'/>
+        <Image src='/images/etherscan.svg' width='40px' height='40px'/>
+
+
       </div>
     </div>
   );
